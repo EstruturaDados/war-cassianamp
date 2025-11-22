eb3b5984a4f8fe413a71d69e52ce5747b368a965
cassianamp <cassianamp@gmail.com>
2025-11-22 17:54:19 +0000

    Entrega do Desafio Nível Novato

--- war-nivel-novato.c
+++ war-nivel-novato.c
@@ -4,6 +4,7 @@
 
 // Inclusão de bibliotecas
 #include <stdio.h>   // Entrada e saída padrão (exs.: printf, scanf)
+#include <string.h>  // Manipulação de strings (ex: strlen, strcpy, strcmp)
 
 // Definição da estrutura Território
 typedef struct {
@@ -15,37 +16,44 @@ typedef struct {
 // Função principal (main)
 int main() {
 
-    // Cadastrando Território 1
-    Territorio territorio1 = {
+    Territorio territorios[5];  //Declaração do vetor para armazenar 5 estruturas do tipo Territorio 
+    int i;  // Definição da variável i para percorrer o laço for
+
+    printf("Vamos cadastrar os 5 territórios iniciais do nosso mundo.\n");
+
+    // Entrada dos dados - Cadastrando os territórios utilizando um laço for
+    for (i = 0; i < 5; i++) {
+
+        printf("\n--- Cadstrando Território %d ---\n", i + 1);
+        
         printf("Nome do território: ");
-        fgets(territorio1.nome, 30, stdin);
+        fgets(territorios[i].nome, 30, stdin);
 
         printf("Cor do exército (ex.: Azul, Verde, Amarelo): ");
-        fgets(territorio1.cor, 10, stdin);
-
+        fgets(territorios[i].cor, 10, stdin);
+        
         printf("Número de tropas: ");
-        scanf("%d", &territorio1.tropas);
-    };
+        scanf("%d", &territorios[i].tropas);
+
+        getchar();  // Para limpar o '\n' deixado no buffer pelo scanf
+       
+    }
+
+    // Feedback de conclusão após o cadastro dos 5 territórios
+    printf("\nCadastro inicial concluído com sucesso!\n");
     
-};
+    // Exibição dos dados - Percorrendo o vetor para exibir os dados de cada território
 
+    printf("\n=======================================\n");
+    printf("     MAPA DO MUNDO - ESTADO ATUAL\n");
+    printf("=======================================\n");
 
-// --- Cadastrando Território 1 ---
-// Nome do território:
-// Cor do exército (ex.: Azul, Verde, Amarelo):
-// Número de tropas: 
+    for (i = 0; i < 5; i++) {
 
-/*
+        printf("\nTERRITÓRIO %d:\n", i + 1);
+        printf("- Nome: %s", territorios[i].nome);
+        printf("- Dominado por: Exército %s", territorios[i].cor);
+        printf("- Tropas: %d\n", territorios[i].tropas);
+    }
 
-Cadastro inicial concluído com sucesso!
-
-=======================================
-     MAPA DO MUNDO - ESTADO ATUAL
-=======================================
-
-TERRITÓRIO 1:
-- Nome:
-- Dominado por:
-- Quantidade de tropas:
-
-*/
+}
